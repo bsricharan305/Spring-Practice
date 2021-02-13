d5538a3af2874f5c1c535f6835921c1b2a666679
sricharan <sricharan245@gmail.com>
2021-02-13 19:51:07 +0530

    day 2 - REST API HandsOn CRUD ops

--- src/main/java/com/example/demo/FormController.java
+++ src/main/java/com/example/demo/FormController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
 
 
 
@@ -59,5 +67,48 @@ public class FormController {
 		mv.addObject(customers);
 		return mv;	
 	}
+		
+		
+	@RequestMapping("/customers")
+	@ResponseBody
+	public List<Customers> getCustomers() {
+	//public String getCustomers() {
+		
+		return repo.findAll();
+	}
+	
+	@RequestMapping("/customers/{cid}")
+	@ResponseBody
+	public Optional<Customers> getCustomersById(@PathVariable("cid") int cid) {
+		
+		return repo.findById(cid);
+	}
+	
+	@PostMapping("/customers")
+	public Customers addCustomer(@RequestBody Customers cust) {
+	//public String getCustomers() {
+		
+		repo.save(cust);
+		return cust;
+	}
+	
+	@DeleteMapping("/customers/{cid}")
+	public Customers deleteCustomer(@PathVariable("cid") int cid) {
+	//public String getCustomers() {
+		
+		Customers c = repo.getOne(cid);
+		
+		repo.delete(c);
+		return c;
+	}
+	
+	@PutMapping(path = "/customers", consumes = {"application/json"})
+	public Customers updateCustomer(@RequestBody Customers cust) {
+	//public String getCustomers() {
+		
+		repo.save(cust);		
+		return cust;
+	}
+	
 	
 }
